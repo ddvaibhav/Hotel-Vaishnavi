45fc42873adf8346e6598a818794cefaf906935c
vaibhav daspute <vaibhavdaspute775@gmail.com>
2026-01-21 21:38:06 +0530

    hii

--- index.tsx
+++ index.tsx
@@ -1,11 +1,5 @@
-
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { name as appName } from './app.json';
 
-// This is the standard entry point for a React Native CLI project
-// The name must match the project name in your Xcode/Android studio configuration
-AppRegistry.registerComponent('VaishnaviHotel', () => App);
-
-// In some preview environments, we might need a fallback, 
-// but for the Xcode project, the above is the source of truth.
-export default App;
+AppRegistry.registerComponent(appName, () => App);
